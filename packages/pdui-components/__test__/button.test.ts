d6db2c67dd2e473e1b75f193f990c9067dfb959f
onemini <1059314550@qq.com>
2023-09-01 23:48:22 +0800

    test：button测试用例的修改

--- packages/pdui-components/__test__/button.test.ts
+++ packages/pdui-components/__test__/button.test.ts
@@ -1,27 +1,27 @@
-import IkButton from '../button'
-import IkIcon from '../icon'
+import PdButton from '../button'
+import PdIcon from '../icon'
 import { describe, expect, test } from 'vitest'
 import { mount } from '@vue/test-utils'
 
-describe('IkButton', () => {
+describe('PdButton', () => {
   test('class', () => {
-    const wrapper = mount(IkButton)
-    expect(wrapper.classes()).toContain('ik-button')
+    const wrapper = mount(PdButton)
+    expect(wrapper.classes()).toContain('pd-button')
   })
   test('class', () => {
-    const wrapper = mount(IkButton, {
+    const wrapper = mount(PdButton, {
       props: {
         type: 'primary',
       },
     })
-    expect(wrapper.classes()).toContain('ik-button-primary')
+    expect(wrapper.classes()).toContain('pd-button-primary')
   })
   test('loading', () => {
-    const wrapper = mount(IkButton, {
+    const wrapper = mount(PdButton, {
       props: {
         loading: true,
       },
     })
-    expect(wrapper.classes()).toContain('ik-button')
+    expect(wrapper.classes()).toContain('pd-button')
   })
 })
